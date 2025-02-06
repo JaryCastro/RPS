31e2eeb91d45c014a3d79511355f912eb3f7b8a1
Jary Castro <jarycastro007@gmail.com>
2025-02-06 09:09:17 -0500

    Added GameState

--- RPS/Sign.swift
+++ RPS/Sign.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
+
 enum Sign {
     case rock
     case paper
@@ -13,6 +25,27 @@ enum Sign {
             return "✋"
         case .scissors:
             return "✌️"
+        }
+    }
+    
+    func compare(_ opponent: Sign) -> GameState {
+        if self == opponent {
+            return .draw
+        }
+        
+        switch self {
+        case .rock:
+            if opponent == .scissors {
+                return .win
+            }
+        case .paper:
+            if opponent == .rock{
+                return .win
+            }
+        case .scissors:
+            if opponent == .paper {
+                return .win
+            }
         }
     }
 }
